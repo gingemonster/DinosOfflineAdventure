628d1b10a353ab8b0b4ca29d788fb7a1bf8fe1df
gingemonster <brian@earthware.co.uk>
2018-10-06 22:07:52 +0100

    got background scrolling for one scene

--- main.c
+++ main.c
@@ -11,14 +11,14 @@ void init();
 void checkInput();
 void checkjumping();
 void updateSwitches();
-void drawdino(BYTE render, BYTE jumping);
+void drawdino(BYTE jumping);
 void drawbg();
 
 UINT8 frame, i, j;
 INT8 jumpindex;
-UINT8 player[2];
+UINT8 playery;
 UINT8 backgroundoffset;
-BYTE hasmovedx,hasmovedy,apressed;
+BYTE hasmovedx,hasmovedy,apressed,running;
 UINT8 jump_array[] = {-26,-12,-6,-3,-1,1,3,6, 12, 26};
 
 unsigned char EmptyBackgroundData[] = 
@@ -36,12 +36,17 @@ void main() {
 		checkInput();
 		updateSwitches();			// Make sure the SHOW_SPRITES and SHOW_BKG switches are on each loop
 		wait_vbl_done();			// Wait until VBLANK to avoid corrupting visual memory		
+		if(running) {
+			drawdino(hasmovedy); // always move dino if moved or not so that we process jump or left right in the same place
+			drawbg(hasmovedx);			
+			delay(60);
+		}
 	}
 	
 }
 
 void cls(){
-	move_bkg(0,4);
+	move_bkg(0,3);
 	for (j=0 ; j != 18 ; j++){
 		for (i=0 ; i != 20 ; i++){		
 			set_bkg_tiles(i,j,1,1,0x0B);
@@ -49,36 +54,36 @@ void cls(){
 	}	
 }
 
-void drawdino(BYTE render, BYTE jumping){
-	if(render){
-		frame = !frame;
-		move_sprite(0,player[0],player[1]);
-		move_sprite(1,player[0] + 8,player[1]);
-		move_sprite(2,player[0] + 16,player[1]);
+void drawdino(BYTE jumping){
 
-		move_sprite(3,player[0],player[1] + 8);
-		move_sprite(4,player[0] + 8,player[1] + 8);
+	frame = !frame;
+	move_sprite(0,64,playery);
+	move_sprite(1,72,playery);
+	move_sprite(2,80,playery);
 
-		if(frame || jumping){
-			set_sprite_tile(5,5); 
-			set_sprite_tile(6,6);
-		}
-		else{
-			set_sprite_tile(5,7); 
-			set_sprite_tile(6,8);
-		}
-		move_sprite(5,player[0] ,player[1] + 16);
-		move_sprite(6,player[0] + 8,player[1] + 16);		
+	move_sprite(3,64,playery + 8);
+	move_sprite(4,72,playery + 8);
+
+	if(frame || jumping){
+		set_sprite_tile(5,5); 
+		set_sprite_tile(6,6);
 	}
+	else{
+		set_sprite_tile(5,7); 
+		set_sprite_tile(6,8);
+	}
+	move_sprite(5,64,playery + 16);
+	move_sprite(6,72,playery + 16);		
+
 }
 
 void drawbg(){
 	set_bkg_tiles(0,10,32,2,map);
+	scroll_bkg(7,0);
 }
 
 void init() {
-	player[0] = 64;
-	player[1] = 80;
+	playery = 80;
 	backgroundoffset = 0;
 	
 	jumpindex = -1;
@@ -97,6 +102,8 @@ void init() {
 	set_sprite_tile(5,5); 
 	set_sprite_tile(6,6);	
 
+		
+
 	cls(); // clear background
 	drawbg();
 	drawdino(1);
@@ -113,7 +120,7 @@ void updateSwitches() {
 void checkjumping(){
 	if(jumpindex > -1){
 
-		player[1] = player[1] + jump_array[jumpindex];
+		playery= playery + jump_array[jumpindex];
 		hasmovedy = 1;
 
 		if(jumpindex == sizeof(jump_array) - 1){
@@ -131,19 +138,9 @@ void checkjumping(){
 }
 
 void checkInput() {
-
-	if(joypad() & J_RIGHT)   // the & rather than and == means that more than one key can be pressed at once     
-	{
-		player[0] = player[0] + 2;
-		hasmovedx = 1;
+	if(joypad() == J_START){
+		running = 1;
 	}
-
-	if(joypad() & J_LEFT)           /* see above */
-	{
-		player[0] = player[0] -2;
-		hasmovedx = 1;
-	}
-
 	if((joypad() & J_A) && apressed == 0) // only want to jump once per keypress
 	{
 		apressed = 1;
@@ -161,17 +158,9 @@ void checkInput() {
 		apressed = 0;
 	}
 
-	drawdino(hasmovedx | hasmovedy, hasmovedy); // always move dino if moved or not so that we process jump or left right in the same place
-	drawbg(hasmovedx);
 
-	if(hasmovedx)
-	{
-		delay(60);	
-		hasmovedx = 0;	
-	}
-	else if (hasmovedy){
-		// if only moved Y (jump) we still want to wait for 30
+	if (hasmovedy){
+		// if only moved Y (jump) we still want to wait for 60
 		hasmovedy = 0;
- 		delay(60);
 	}
 }
