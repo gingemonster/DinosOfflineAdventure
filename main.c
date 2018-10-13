14115c3d4a605ff26fb6623eec346affd2a8afb5
gingemonster <brian@earthware.co.uk>
2018-10-13 18:26:35 +0100

    getting close on obstacle initial render

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <gb/gb.h>
 #include <stdio.h>
 #include <string.h>
+#include <rand.h>
 #include "sprites.h"
 #include "sprites.c"
 #include "background.h"
@@ -18,6 +19,7 @@ struct PG {
 	UBYTE x;
 	UBYTE y;
 	UBYTE graphic;
+	UBYTE initialized;
 };
 
 void init();
@@ -34,20 +36,22 @@ void setupinitialsprites();
 void setupinitialbackground();
 UBYTE shouldrenderanimationframe();
 UINT8 getscreenquadrant(UINT8 screenoffset);
-UINT8 setupcharactersprites(struct PG* character);
+void setupcharactersprites(struct PG* character);
 void movecharactersprites(struct PG* character);
+void generatequadrantobstacles(INT8 quadrant);
 
 const unsigned char blankmap[]={0x00};
 const UINT8 jump_array[] = {-26,-6,-3,-1,1,3,6, 26};
 const UBYTE dinospritemap[] = {0,1,2,3,4,255,5,6,255}; // use 255 to indicate none as there is no concept of array null values, they would eval to 0
+const UBYTE smallcactispritemap[] = {11,255,255,10,255,255,255,255,255}; // use 255 to indicate none as there is no concept of array null values, they would eval to 0
+const UBYTE largecactispritemap[] = {9,255,255,10,255,255,255,255,255}; // use 255 to indicate none as there is no concept of array null values, they would eval to 0
 const INT8 speed = 2;
 UINT8 skipframesforspriteanim;
 UINT16 lastscreenquadrantrendered,currentscreenquadrant,nextscene,screenpixeloffset;
 UINT8 frame,lastspriteid;
-INT8 h,i,j,jumpindex;
+INT8 h,i,j,k,jumpindex;
 UBYTE hasmovedy,apressed,running;
 UINT8 enemysprites[];
-
 
 struct PG obstacles[8]; // they will all be on same line and gameboy can only do 10 sprites on a line, dino takes 2
 struct PG dino;
@@ -93,42 +97,46 @@ void drawdino(UBYTE jumping){
 			set_sprite_tile(6,6);
 		}
 		else{
-				set_sprite_tile(5,7); 
-				set_sprite_tile(6,8);
-				playstep();
+			set_sprite_tile(5,7); 
+			set_sprite_tile(6,8);
+			playstep();
 		}
 	}
 
 	movecharactersprites(&dino);
 
 }
+// void drawcacti(UINT8 x, UINT8 y, UINT8 spritenum){
+// 	set_sprite_tile(spritenum,9); 
+// 	set_sprite_tile(spritenum+1,10);
+// 	move_sprite(spritenum,x,y);
+// 	move_sprite(spritenum+1,x,y+8);
+// 	enemysprites[0] = spritenum;
+// 	enemysprites[1] = spritenum + 1;
+// }
 
 void movecharactersprites(struct PG* character){
 	h = 0; // h needed to keep track of sprite maps that are not empty
-	for(i=0;i!=3;i++){
-		for(j=0;j!=3;j++){
-			// increment characters sprite map through all 9 sprite maps, checking if any should be ignored
-			if(character->spritemapids[(i * 3 + j)] != 255){
-				// 
-				move_sprite(character->startspriteid + h,character->x + (j*8),character->y + (i*8));
-				h++;
+	
+	// dont bother moving a character if its not initialized, probably empty obstacle
+	if(character->initialized==1){
+		//printf("%u %u \n", (UINT16)character->startspriteid, (UINT16)character->x);
+		for(i=0;i!=3;i++){
+			for(j=0;j!=3;j++){
+				// increment characters sprite map through all 3x3 grid of sprite maps, checking if any should be ignored
+				if(character->spritemapids[(i * 3 + j)] != 255){
+					move_sprite(character->startspriteid + h,character->x + (j*8),character->y + (i*8));
+					h++;
+				}
 			}
 		}
 	}
 }
 
-void drawcacti(UINT8 x, UINT8 y, UINT8 spritenum){
-	set_sprite_tile(spritenum,9); 
-	set_sprite_tile(spritenum+1,10);
-	move_sprite(spritenum,x,y);
-	move_sprite(spritenum+1,x,y+8);
-	enemysprites[0] = spritenum;
-	enemysprites[1] = spritenum + 1;
-}
-
 void scrollbgandenemies(){
 	// scroll bg by speed
 	scroll_bkg(speed,0);
+
 	// update the current count of how far we have scrolled the screen
 	screenpixeloffset += speed;
 
@@ -174,6 +182,191 @@ UINT8 getscreenquadrant(UINT8 screenoffset){
 	}
 	return 3;	
 }
+
+// =========================================================
+// Initialisation functions at very start of game
+// =========================================================
+
+void init() {
+	skipframesforspriteanim = speed * 6;
+	jumpindex = -1;
+	screenpixeloffset = 0;
+
+	setupinitialbackground(); // create initial background
+	setupinitialsprites(); // create initial sprites
+
+	drawdino(1);
+
+	// move all 8 obstacles to start positions
+	for(k=0;k!=8;k++){
+		movecharactersprites(&obstacles[k]);
+	}
+	
+	//drawcacti(90,81,7);
+	enablesound();
+}
+
+void setupinitialsprites(){
+	set_sprite_data(0, 12, SpritesData);   /* defines the sprite data for all sprites */
+
+	// create dino character
+	// copy array of dinos sprite ids into dino.ids using memcpy
+	memcpy(dino.spritemapids,dinospritemap, sizeof(dinospritemap)); 
+	dino.x = 14;
+	dino.y = 80;
+	dino.startspriteid = 0;
+	setupcharactersprites(&dino); // & is saying pass a pointer to the function
+
+	// generate initial obstacles for all 4 quadrants of VRAM
+	// generatequadrantobstacles(0);
+	generatequadrantobstacles(1);
+	generatequadrantobstacles(2);
+	generatequadrantobstacles(3);
+}
+
+void generatequadrantobstacles(INT8 quadrant){
+	// get start possition for this set of obstacles
+	// for offsetting them from the start of the quadrant position
+	// dont want one with 10 of the start or end of quadrant
+	UINT8 xoffsetfromstart; 
+	UINT8 currentindex;
+	INT8 randomnum;
+	initrand(DIV_REG);
+	xoffsetfromstart =  (rand() % 45) + 10 + ((INT16)quadrant * 64);
+
+	// loop to create up to 2 obstacles in this quadrant
+	// use j as i is used by setupcharactersprites
+	for(j=0;j!=2;j++){
+		// get random number between 0 and 2
+		// used to decide what obstacle to generate
+		initrand(DIV_REG);
+		randomnum = rand() % 2;
+		currentindex = (INT16)quadrant * 2 + j;
+		
+		// if randomnum is 2 then skip creating an obstacle
+		if(randomnum!=2){
+
+			obstacles[currentindex].startspriteid =  9 + (currentindex*2); // dino takes 9, each character takes 2 sprites
+			
+			obstacles[currentindex].x = (xoffsetfromstart + (j * 8)); // 2nd obstacle will always be 8 pixels to the right of first
+			obstacles[currentindex].y = 81;
+
+			//printf("sid %u x %u obsi %u\n", (UINT16)obstacles[currentindex].startspriteid, (UINT16)obstacles[currentindex].x,(UINT16)currentindex);
+
+			if(randomnum==0){
+				//set large cacti sprite map
+				memcpy(obstacles[currentindex].spritemapids,largecactispritemap, sizeof(largecactispritemap));
+			}
+			else if (randomnum==1){
+				//set small cacti sprite map
+				memcpy(obstacles[currentindex].spritemapids,smallcactispritemap, sizeof(largecactispritemap));
+			}
+
+			// load the map images into sprites overwriting any previous ones
+			setupcharactersprites(&obstacles[currentindex],obstacles[currentindex].startspriteid);
+			
+		}
+		else{
+			obstacles[currentindex].initialized = 0; // empty
+		}
+	}
+}
+
+void setupcharactersprites(struct PG* character){
+	//loop map ids and load sprites where there is an id
+	// -> are saying get a property from a pointer
+	UBYTE currentspriteid = character->startspriteid;
+
+	for(i=0;i!=sizeof(character->spritemapids);i++){
+		if(character->spritemapids[i]!=255){
+			//printf("i %u sid %u smap %u\n",(UINT16)i,(UINT16)currentspriteid,(UINT16)character->spritemapids[i]);
+			set_sprite_tile(currentspriteid,character->spritemapids[i]);
+			currentspriteid++;
+		}
+		
+	}
+	character->initialized = 1;
+}
+
+void setupinitialbackground(){
+	cls(); // clear background so that all tiles are blank to start with
+
+	set_bkg_data(0, 11, BackgroundData); // load background data into tileset
+	set_bkg_tiles(0,10,32,1,map); // draw first background row
+	set_bkg_tiles(0,11,32,1,&map[96]); // draw secind background row	
+
+	nextscene = 1; // set next backgroundscene to draw
+	screenpixeloffset = 0; // set initial screen scroll offset
+	lastscreenquadrantrendered = 0; // set the last screen quadrant rendered
+}
+
+void updateSwitches() {
+	DISPLAY_ON;	
+	HIDE_WIN;
+	SHOW_SPRITES;
+	SHOW_BKG;
+}
+
+// =========================================================
+// Animation utils
+// =========================================================
+UBYTE shouldrenderanimationframe(){
+	return screenpixeloffset % skipframesforspriteanim == 0;
+}
+
+// =========================================================
+// Event handlers
+// =========================================================
+
+void checkjumping(){
+	if(shouldrenderanimationframe()){ // TODO MOVE TO SHARED FUNCTION
+		if(jumpindex > -1){
+			dino.y= dino.y + jump_array[jumpindex];
+			hasmovedy = 1;
+
+			if(jumpindex == sizeof(jump_array) - 1){
+				// at end of jump index so end
+				jumpindex = -1;
+			}
+			else{
+				// move to next jump ammount
+				jumpindex ++;
+			}
+		}
+		else{
+			hasmovedy = 0;
+		}
+	}
+}
+
+void checkInput() {
+	if(joypad() == J_START){
+		running = 1;
+	}
+	if((joypad() & J_A) && apressed == 0 && running) // only want to jump once per keypress
+	{
+		apressed = 1;
+		// dont jump if already jumping
+		if(jumpindex==-1){
+			playjump();
+			jumpindex = 0;
+		}
+	}
+	else if ((joypad() & J_A) && apressed == 1)
+	{
+		// do nothing user is still pressing A
+	}
+	else
+	{
+		apressed = 0;
+	}
+
+	if (hasmovedy){
+		// if only moved Y (jump) we still want to wait for 60
+		hasmovedy = 0;
+	}
+}
+
 
 // =========================================================
 // Sound functions
@@ -254,137 +447,4 @@ void playstep(){
 	NR42_REG = 0x11;
 	NR43_REG = 0x00;
 	NR44_REG = 0xC0;
-}
-
-// =========================================================
-// Initialisation functions at very start of game
-// =========================================================
-
-void init() {
-	skipframesforspriteanim = speed * 6;
-	jumpindex = -1;
-	screenpixeloffset = 0;
-
-	setupinitialbackground(); // create initial background
-	setupinitialsprites(); // create initial sprites
-
-	drawdino(1);
-	drawcacti(90,81,7);
-	enablesound();
-}
-
-void setupinitialsprites(){
-	set_sprite_data(0, 12, SpritesData);   /* defines the sprite data */
-
-	// create dino character
-	// copy array of dinos sprite ids into dino.ids using memcpy
-	memcpy(dino.spritemapids,dinospritemap, sizeof(dinospritemap)); 
-	dino.x = 14;
-	dino.y = 80;
-	dino.startspriteid =  setupcharactersprites(&dino); // & is saying pass a pointer to the function
-
-	// TODO create first random set of obstacles
-	
-
-}
-
-UINT8 setupcharactersprites(struct PG* character){
-	//loop map ids and load sprites where there is an id
-	// -> are saying get a property from a pointer
-	UBYTE firstspriteid;
-	if(lastspriteid==NULL){
-		firstspriteid = 0;
-		lastspriteid = 0;
-	}
-	else{
-		firstspriteid = lastspriteid;
-	}	
-	for(i=0;i!=sizeof(character->spritemapids);i++){
-		if(character->spritemapids[i]!=255){
-			set_sprite_tile(lastspriteid,character->spritemapids[i]);
-		}
-		lastspriteid++;
-	}
-
-	// return index of first sprite for this object
-	return firstspriteid;
-}
-
-void setupinitialbackground(){
-	cls(); // clear background so that all tiles are blank to start with
-
-	set_bkg_data(0, 11, BackgroundData); // load background data into tileset
-	set_bkg_tiles(0,10,32,1,map); // draw first background row
-	set_bkg_tiles(0,11,32,1,&map[96]); // draw secind background row	
-
-	nextscene = 1; // set next backgroundscene to draw
-	screenpixeloffset = 0; // set initial screen scroll offset
-	lastscreenquadrantrendered = 0; // set the last screen quadrant rendered
-}
-
-void updateSwitches() {
-	DISPLAY_ON;	
-	HIDE_WIN;
-	SHOW_SPRITES;
-	SHOW_BKG;
-}
-
-// =========================================================
-// Animation utils
-// =========================================================
-UBYTE shouldrenderanimationframe(){
-	return screenpixeloffset % skipframesforspriteanim == 0;
-}
-
-// =========================================================
-// Event handlers
-// =========================================================
-
-void checkjumping(){
-	if(shouldrenderanimationframe()){ // TODO MOVE TO SHARED FUNCTION
-		if(jumpindex > -1){
-			dino.y= dino.y + jump_array[jumpindex];
-			hasmovedy = 1;
-
-			if(jumpindex == sizeof(jump_array) - 1){
-				// at end of jump index so end
-				jumpindex = -1;
-			}
-			else{
-				// move to next jump ammount
-				jumpindex ++;
-			}
-		}
-		else{
-			hasmovedy = 0;
-		}
-	}
-}
-
-void checkInput() {
-	if(joypad() == J_START){
-		running = 1;
-	}
-	if((joypad() & J_A) && apressed == 0 && running) // only want to jump once per keypress
-	{
-		apressed = 1;
-		// dont jump if already jumping
-		if(jumpindex==-1){
-			playjump();
-			jumpindex = 0;
-		}
-	}
-	else if ((joypad() & J_A) && apressed == 1)
-	{
-		// do nothing user is still pressing A
-	}
-	else
-	{
-		apressed = 0;
-	}
-
-	if (hasmovedy){
-		// if only moved Y (jump) we still want to wait for 60
-		hasmovedy = 0;
-	}
 }
